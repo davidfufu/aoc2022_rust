25ae340b3b0f378fd86a30a4e059d1ad10dcd7cb
David Fu <wangxingdavidfu@fb.com>
2023-05-03 16:34:44 -0400

    all

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod 1 {
+    
+}
+
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader };
+
+
+
+fn main() {
+    calorie_counting()
+}
+
+
+
+fn calorie_counting(){
+    let file_name = "./calories.txt";
+    let file = File::open(file_name).unwrap();
+    let lines_interator = io::BufReader::new(file).lines(); 
+
+    for line in lines_interator{
+        println!("{}", line.unwrap());
+    }
+}
